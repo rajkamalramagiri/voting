2f9db84b8ee437dfd5925fab9ca130dbdeae6060
rajkamalramagiri <rajkamal.smit@gmail.com>
2020-09-15 19:12:09 +0530

    make it better

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+class Vote extends Component {
+  state = {
+    content: [
+      { count: 1, subject: 'Php', id: 1 },
+      { count: 1, subject: 'Pythhon', id: 2 },
+      { count: 1, subject: 'Go', id: 3 },
+      { count: 1, subject: 'Java', id: 4 },
+    ],
+  }
+  handleClick = (contentn) => {
+    const content = [...this.state.content]
+    const index = content.indexOf(contentn)
+    console.log(index)
+    content[index].count++
+    this.setState({ content })
+    console.log(this.state.content[index].count)
+  }
+
+  render() {
+    return (
+      <div className="divClass">
+        <h1>Vote Your Language!</h1>
+        <span className="spanClass">
+          {this.state.content.map((x) => {
+            return (
+              <div>
+                <table className="tclass">
+                  <tr className="trclass">
+                    <td className="tdclass">{x.count}</td>
+                    <td className="tdclass">{x.subject}</td>
+                    <td className="tdclass">
+                      <button
+                        className="buttonclass"
+                        onClick={() => this.handleClick(x)}
+                      >
+                        Click Here
+                      </button>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            )
+          })}
+        </span>
+      </div>
+    )
+  }
+}
+
+export default Vote
---
+++ src/Todo.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+class Todo extends Component {
+  state = {
+    a: ['lunch', 'dinner'],
+    b: '',
+  }
+  handleChange = (e) => {
+    const x = e.target.value
+    this.setState({ b: x })
+    // this.setState({ a: [1, 2] })
+  }
+  onDelete = (e) => {
+    const x = e.target.value
+    const list = [...this.state.a]
+    list.splice(x, 1)
+    //delete list[x]
+    this.setState({ a: list })
+  }
+
+  handleClick = () => {
+    this.setState({ a: [...this.state.a, this.state.b] })
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Todo list </h1>
+        {this.state.a.map((x, i) => (
+          <div>
+            <p>
+              {x}
+              {`     `}
+              <button onClick={this.onDelete} value={i}>
+                delete
+              </button>
+            </p>
+          </div>
+        ))}
+        <label>
+          {' '}
+          add your duty
+          <br />
+        </label>
+        <input type="text" onChange={this.handleChange} name="todo" />
+        <button type="submit" onClick={this.handleClick}>
+          Add to list
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Todo
